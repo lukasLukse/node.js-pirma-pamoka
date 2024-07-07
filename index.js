930453bd6afaa06aa829f7105096219643af3553
unknown <lukas.lasas1@gmail.com>
2024-07-07 16:50:01 +0300

    Su visais update

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import carRouter from "./src/router/car.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(carRouter);
+
+app.use((req, res) => {
+  return res.status(404).send("Sorry, endpoint does not exist");
+});
+
+app.listen(3000, () => {
+  console.log("Your application was launched successfully on port 3000.");
+});
---
+++ src/router/car.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import {
+  GET_CARS,
+  GET_CAR_BY_ID,
+  DELETE_CARS,
+  INSERT_CARS,
+  UPDATE_CAR_BY_ID,
+} from "../controller/car.js";
+
+const router = express.Router();
+
+router.get("/cars", GET_CARS);
+
+router.get("/cars/:id", GET_CAR_BY_ID);
+
+router.put("/cars/:id", UPDATE_CAR_BY_ID);
+
+router.post("/cars", INSERT_CARS);
+
+router.delete("/cars", DELETE_CARS);
+
+export default router;
