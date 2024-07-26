3c4927acff66e57003cdac6bbb3c4f12b90bc61b
unknown <lukas.lasas1@gmail.com>
2024-07-26 21:05:05 +0300

    Added login/Auth

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import UserModel from "../model/user.js";
+import jwt from "jsonwebtoken";
 
 const SIGN_UP = async function (req, res) {
   try {
@@ -19,12 +20,12 @@ const SIGN_UP = async function (req, res) {
       password: hash,
     };
 
-    const cars = await new UserModel(user);
+    const newUser = await new UserModel(user);
 
     cars.save();
 
     return res.status(200).json({
-      cars: cars,
+      cars: newUser,
       message: "user was saved successfully.",
     });
   } catch (err) {
@@ -33,4 +34,30 @@ const SIGN_UP = async function (req, res) {
   }
 };
 
-export { SIGN_UP };
+const LOGIN = async function (req, res) {
+  const user = await UserModel.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.status(403).json({ message: "Bad Auth." });
+  }
+
+  const isPasswordMatch = bcrypt.compareSync(req.body.password, user.password);
+
+  if (!isPasswordMatch) {
+    return res.status(403).json({ message: "Bad Auth." });
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      userId: user.id,
+    },
+    "PASSWORD123",
+    { expiresIn: "12h" }
+  );
+
+  return res
+    .status(200)
+    .json({ token: token, message: "login was succesfull" });
+};
+export { SIGN_UP, LOGIN };
